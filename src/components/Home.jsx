6557bb1832e71de513d8f6a8c1a674e96e6a88e7
elene638 <elenejavaxishvili@yahoo.com>
2022-08-27 13:38:34 -0700

    Home added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import logo from "../images/Logo.png";
+import computer from "../images/home-picture.png";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="logo">
+        <img alt="logo" src={logo} />
+      </div>
+      <div className="computer-picture">
+        <img alt="pic" src={computer} />
+      </div>
+      <div className="button-container">
+        <button className="btn">ჩანაწერების დამატება</button>
+        <button className="btn">ჩანაწერების სია</button>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
